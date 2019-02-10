65b8c552c0fa52a22d6503ea3232bf45c5a4e6ca
donchev1 <adonchev111@gmail.com>
2019-02-10 19:31:04 +0000

    select-list-items

--- Organiser.Data/Models/User.cs
+++ Organiser.Data/Models/User.cs
@@ -2,7 +2,7 @@
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Web.Mvc;
 
 namespace Organiser.Data.Models
 {
