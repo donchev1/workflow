f5283f199ee692dc9064fd1de89dd61852045d97
donchev1 <adonchev111@gmail.com>
2019-02-20 08:48:39 +0000

    Department State controller refactored to use action layer
    Needs testing

--- Organiser/Actions/ActionObjects/ActionObjects.cs
+++ Organiser/Actions/ActionObjects/ActionObjects.cs
@@ -1,27 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Organiser.Actions.ActionObjects
 {
-    public class AccountActionObject
+    public class ActionObject
     {
+        public bool Success { get; set; }
         public string ErrorMessage { get; set; }
-        public bool Success { get; set; } = true;
         public bool RedirectToError { get; set; }
-    }
-
-    public class CreateObject : AccountActionObject
-    {
-        public bool UserCreated { get; set; }
-        public bool UserIsAdmin { get; set; }
-    }
-
-    public class LoginActionObject
-    {
-        public bool UserExists { get; set; }
-        public ClaimsPrincipal ClaimsObject { get; set; }
     }
 }
---
+++ Organiser/Actions/ActionObjects/DepartmentStateActionObjects.cs
@@ -0,0 +1,16 @@
+﻿using Organiser.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Organiser.Actions.ActionObjects
+{
+  
+
+    public class MarkAsReadyActionObject : ActionObject
+    {
+        public Dictionary<string, string> Result { get; set;}
+        public DepartmentState DepartmentState { get ;set;}
+    }
+}
